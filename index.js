4eb67fd3a63fb4badd0d83aa6d57c74c21fd975d
Salu Kumari <91481350+salu6066@users.noreply.github.com>
2024-12-26 10:31:00 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// console.log("hello world");
+import express from 'express'
+
+const app = express();
+
+app.listen(8080,()=>{
+    console.log("server started at port 8080")
+
+})
+app.get('/',(req,res)=>{
+    res.send("hello world seven")
+})
+
+app.get('/home',(req,res)=>{
+    res.send("this is home API")
+
+})
+app.get('/products',(req,res)=>{
+    let products =
+    [
+        {
+        "prod_name" : "Apple Watch",
+        "prod_price": $25,
+        },
+        {
+        "prod_name" : "Samsung Watch",
+        "prod_price": $20
+        }
+
+
+    ]
+    // res.send("the list od products are: ")
+    res.json(products)
+
+})
